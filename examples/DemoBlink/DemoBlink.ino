cae32312018658390d12a54ce5de546534ec7d6c
Celso Eiju Ito <eijuito@gmail.com>
2018-04-10 15:04:08 -0300

    Fix issues and comments
    Fixed comments
    Fixed setOnChangeFunction
    Fixed setOnTimeoutFunction

--- examples/DemoBlink/DemoBlink.ino
+++ examples/DemoBlink/DemoBlink.ino
@@ -1,7 +1,5 @@
-#include <EEPROM.h>
 #include <avr/pgmspace.h>
-#include <avr/wdt.h>
-#include <IToSmartMenu.h>
+#include "IToSmartMenu.h"
 
 char aux[33];
 
@@ -9,15 +7,17 @@ char aux[33];
 IToSmartMenu menu;
 
 // itens do menu
-IToSmartMenu_text text_0_0[] = "Menu item 1";
-IToSmartMenu_item item_0_0 = {(char*)text_0_0, 1, 0x00, 0};
+IToSmartMenu_text text_0_0[] = "Menu item 1"; // texto do item 1 do menu
+IToSmartMenu_item item_0_0 = {(char*)text_0_0, // link para o texto
+                                            1, // tempo em segundos da permanencia neste item
+                                         0x00, // Flags não são usados neste exemplo
+                                           0}; // Sempre zero
 
-IToSmartMenu_text text_1_0[] = "Menu item 2";
-IToSmartMenu_item item_1_0 = {(char*)text_1_0, 1, 0x01, 0};
-
-// item de mensagem - aparece temprariamento conforme o tempo de timeout
-IToSmartMenu_text text_mensagem[] = "Processando";
-IToSmartMenu_item item_mensagem = {(char*)text_mensagem, 3, 0x00, 0};
+IToSmartMenu_text text_1_0[] = "Menu item 2"; // texto do item 2 do menu
+IToSmartMenu_item item_1_0 = {(char*)text_1_0, // link para o texto
+                                            1, // tempo em segundos da permanencia neste item
+                                         0x01, // Flags não são usados neste exemplo
+                                           0}; // Sempre zero
 
 int acendeLED() { // funcão chamada ao mover para o item 1
   digitalWrite(LED_BUILTIN, HIGH);
@@ -33,8 +33,6 @@ int apagaLED() { // funcão chamada ao mover para o item 2
   return 0;
 }
 
-int nop() {}
-
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
@@ -42,21 +40,20 @@ void setup() {
   while(!Serial) {}
   menu.version(aux);
   Serial.println(aux);
-  menu.setOnChangeFunction(nop);
-  menu.setOnTimeoutFunction(nop);
 
   // define eventos para movimento automatico por timeout
-  menu.setEvent(&item_0_0, menu.EVENT_TIMEOUT, &item_1_0);
-  menu.addOpenFunction(&item_0_0, acendeLED);
+  menu.setEvent(&item_0_0, menu.EVENT_TIMEOUT, &item_1_0); // para o item 0_0, ao ocorrer timeout, vá para o item 1_0
+  menu.addOpenFunction(&item_0_0, acendeLED); // Ao abrir o item 0_0, acende o LED
 
-  menu.setEvent(&item_1_0, menu.EVENT_TIMEOUT, &item_0_0);
-  menu.addOpenFunction(&item_1_0, apagaLED);
+  menu.setEvent(&item_1_0, menu.EVENT_TIMEOUT, &item_0_0); // para o item 0_0, ao ocorrer timeout, vá para o item 1_0
+  menu.addOpenFunction(&item_1_0, apagaLED); // Ao abrir o item 0_0, apaga o LED
 
-  menu.setItem(&item_1_0);
+  menu.setItem(&item_1_0); // inicie o ciclo com o item 1_0 selecionado
 }
 
 void loop() {
   // chama as funções do menu para executar funcões durante o loop
   menu.onLoop(0);
 }
+
 
